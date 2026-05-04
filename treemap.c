984e4348bc721dc2cc2a854a8dfafe57627e0afc
Your Name <you@example.com>
2026-05-04 03:16:46 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -328,7 +328,11 @@ Pair * nextTreeMap(TreeMap * tree) {
         parent= parent->parent;
     }
     tree->current= parent;
-    return parent->pair;
+    if(parent != NULL)
+    {
+        return parent->pair;
+    }
+    return NULL;
 }
 
 // 7. La función Pair* upperBound(TreeMap* tree, void* key) retorna el Pair con clave igual a key. 
